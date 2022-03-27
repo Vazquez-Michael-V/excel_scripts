37b19f026a5083f7a8b2f6b6bab812091e2a6f5c
Michael Vazquez <75464767+Vazquez-Michael-V@users.noreply.github.com>
2022-03-27 00:08:25 -0400

    Allows contents from multiple columns.
    A specified column can be searched for contents in one or more other columns.

--- cols_contains_1.py
+++ cols_contains_1.py
@@ -1,46 +1,57 @@
 
 import pandas as pd
 
-def cols_substring(col_to_search, col_substring, cols_substring_filename, cols_substring_output_filename=None):
-    """Checks if a column contains a substring from another column. \n
-    'col_to_search' will be searched for substrings in 'col_substring'. \n
-    'cols_substring_filename' (required argument) is the excel file to be read, and
-    'cols_substring_output_filename' (optional argument) is the excel file that will print the results of the
-    substring search. If 'cols_substring_output_filename' is not specified, then no excel file will be output.   
+def find_contents(specified_column, substring_columns, excel_filename, results_filename=None):
+    """Useful when needing to know if contents in one or more columns also appear in a specified column.\n
+    Searches specified column for contents in other column(s).\n
+    substring_columns is a list of one or more column names appearing in excel_filename.\n
+    Argument results_filename is optional. If not given, then no excel file will be created.
     """
     
-    df_contains = pd.read_excel(cols_substring_filename)
+    df_main_file = pd.read_excel(excel_filename)
+    
+    print(df_main_file)
+    
+    # Get a list of unique search values.
+    df_search_cols = df_main_file[substring_columns]       
+    search_values = []
+    for col in substring_columns:
+        df_temp_sv = df_search_cols[col].drop_duplicates()
+        for elements in list(df_temp_sv):
+            search_values.append(elements)
+    
+    print(search_values)
     
     big_found_list = []
     df_results = pd.DataFrame()
-    # Search only once for each value in the Search column.
-    for i in sorted(list(set(list(df_contains[col_substring])))):
-        print(f"Searching for '{i}'.")
-        df_row = df_contains[df_contains[col_to_search].str.contains(i)]
-        df_results = df_results.append(df_row)
-        found_list = [i for x in range(len(list(df_row[col_to_search])))]
+    for s in search_values:
+        print(f"Searching for '{s}'.")
+        df_temp_search = df_main_file[df_main_file[specified_column].str.contains(s)]
+        # print(df_temp_search)
+        df_results = df_results.append(df_temp_search)
+        found_list = [s for x in range(len(list(df_temp_search[specified_column])))]
         big_found_list.append(found_list)
-    
-    # Add Results column to allow for grouping.
-    found_col = []
-    for small_list in big_found_list:
-        for s in small_list:
-            found_col.append(s)
-    df_results['Results'] = found_col
-    
+        
+        # Add Results column to allow for grouping.
+        found_col = []
+        for small_list in big_found_list:
+            for s in small_list:
+                found_col.append(s)
+        df_results['Results'] = found_col
     
     # Clean up df_results.
-    df_results = df_results.sort_values(by=[col_to_search]).reset_index(drop=True)
-    df_results = df_results.drop(columns=[col_substring])    
+    df_results = df_results.sort_values(by=[specified_column]).reset_index()
+    df_results = df_results.drop(columns=substring_columns)    
     
-    if cols_substring_output_filename != None:
-        with pd.ExcelWriter(cols_substring_output_filename) as writer:
-            df_results.to_excel(writer, index=False)
+    if results_filename != None:
+        with pd.ExcelWriter(results_filename) as writer:
+            df_results.to_excel(writer, index=False)       
+        
 
     return df_results
     
-        
-a = cols_substring('Chairs', 'Search', 'contains_file.xlsx', 'excel_contains.xlsx')
+
+a = find_contents('Foo', ['Search_1', 'Search_2'], 'contains_file.xlsx', 'results.xlsx')
 
 print(a)
 print(type(a))
