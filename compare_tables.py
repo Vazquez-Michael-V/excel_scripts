097f51cab53346544bba0a4945801eef3272493a
Michael Vazquez <75464767+Vazquez-Michael-V@users.noreply.github.com>
2022-05-23 23:51:49 -0400

    Addressing DataFrames of different shapes.

--- compare_tables.py
+++ compare_tables.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
 df_left = pd.DataFrame(data={
-    'Col_1': [1,2,3,0,4,9,22,8], # Merging left_on.
-    'Col_2': [5,6,7,4,5,0,31,-8], 
-    'Col_5': [0,2,3,0,-1,8,4,0]                             
+    'Col_1': [1,2,3,0,4,9,22], # Merging left_on.
+    'Col_2': [5,6,7,4,5,0,31], 
+    'Col_5': [0,2,3,0,-1,8,4]                             
     })
 
 # print(df_left)
 # print(f'df_left shape: {df_left.shape}')
 
 df_right = pd.DataFrame(data={
-    'Col_3': [1,2,10,4,8,9,22,0], # Merging right_on.
-    'Col_4': [5,6,7,4,5,0,40,0],       
+    'Col_3': [1,2,10,4,8,9,22,0,24], # Merging right_on.
+    'Col_4': [5,6,7,4,5,0,40,0,70],       
     })
 
 # print(df_right)
@@ -31,8 +31,6 @@ left_list = list(df_left['Col_1'])
 merged_left = list(df_merge['Col_1'])
 
 excluded_left_list = []
-# Only need to do the row_excluded list on either the left table or the right table.
-# Result will be the same, since the row number is the same in both tables.
 row_excluded_list = []
 for i in left_list:
     if i in merged_left:
@@ -67,6 +65,24 @@ excluded_dict['Excluded_Right'] = excluded_right_list
 print(excluded_dict)
 
 # TODO: This DataFrame requires same length.
+print(excluded_dict)
+
+len_left = len(excluded_dict['Excluded_Left'])
+print(f'len_left is: {len_left}')
+
+len_right = len(excluded_dict['Excluded_Right'])
+print(f'len_right is: {len_right}')
+
+if len_left - len_right == 0:
+    pass
+elif len_left - len_right < 0:
+    excluded_dict['Excluded_Left'] = excluded_dict['Excluded_Left'] + [0]*abs(len_left - len_right)
+elif len_left - len_right > 0:
+    excluded_dict['Excluded_Right'] = excluded_dict['Excluded_Right'] + [0]*abs(len_left - len_right)
+
+print(excluded_dict)
+       
+
 # df_excluded_values = pd.DataFrame(data=excluded_dict)
 # print(df_excluded_values)
 
@@ -118,12 +134,13 @@ print(df_excluded)
 print(f'df_excluded shape is {df_excluded.shape}')
 print("\n")
 
-print("Rows excluded when merging df_left and df_right, with left_on=['Col_1'] and right_on=['Col_3']:")
+print("Rows number(s) excluded when merging df_left and df_right, with left_on=['Col_1'] and right_on=['Col_3']:")
 for i in set(list(df_excluded['Row_Number'])):
     print(i)
 
 print("\n")
 # TODO: This DataFrame requires same length.
+print(excluded_dict)
 # df_excluded_simple = pd.DataFrame(data=excluded_dict)
 # print(df_excluded_simple)
 
